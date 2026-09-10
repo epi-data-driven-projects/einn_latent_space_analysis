124c72846c042d6d93f236024efe008702ccbf8c
zoltanocska <zolysta@gmail.com>
2026-09-10 02:08:43 +0200

    fixed docstring

--- einn/model/interface/ode_solution.py
+++ einn/model/interface/ode_solution.py
@@ -12,7 +12,7 @@ class ODESolution:
         Shape: [Batch, Seq_len, d_s] (e.g., d_s = 5 for SEIRM, d_s = 3 for SIR)
     :param torch.Tensor params: Bounded, time-dependent physical parameters (e.g., beta, gamma)
         Shape: [Batch, Seq_len, d_p] (e.g., d_p = 4 for SEIRM, d_p = 2 for SIR)
-    :param torch.Tensor t: The time vector corresponding to the solution.
+    :param Optional[torch.Tensor] t: The time vector corresponding to the solution.
         Shape:[Batch, Seq_len, 1].
     """
     ds_dt: torch.Tensor
